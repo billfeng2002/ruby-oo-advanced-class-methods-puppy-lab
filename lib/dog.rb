db61525ea08d65b9b01708a013cd2fbcfb658617
Bill <billfeng2002@gmail.com>
2021-06-09 10:56:59 -0700

    commit tree 5dc6489b1108e8e4f07f9c53e2b27e077f4a8040

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
 # Add your code here
+class Dog
+  @@all=[]
+  WALUIGI=10
+  attr_accessor :name
+  def initialize(name)
+    self.name=name
+    self.save
+  end
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    puts self.all.map{|dog| dog.name}
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.clear_all
+    self.all.clear
+  end
+end
